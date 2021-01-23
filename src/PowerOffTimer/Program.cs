5e7f778d7ce86160b937c50e63786c3c671714f3
Alexander Liakhovets <alexanderliakhovets@gmail.com>
2021-01-24 01:33:36 +0300

    Update sdk to net5.0 and Avalonia 0.10.0 (#2)
    * update sdk to net5.0 and Avalonia 0.10.0
    
    * Update dotnet-core.yml
    
    * fix import
    
    * Update dotnet-core.yml

--- src/PowerOffTimer/Program.cs
+++ src/PowerOffTimer/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Logging.Serilog;
+﻿using Avalonia;
 using Avalonia.ReactiveUI;
 
 namespace PowerOffTimer
@@ -18,7 +15,7 @@ namespace PowerOffTimer
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .LogToDebug()
+                .LogToTrace()
                 .UseReactiveUI();
     }
 }
